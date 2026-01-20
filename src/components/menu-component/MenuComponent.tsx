108624d78182ae08b642fff8b54c8f1a02c3437b
Vladyslav_Hrytsak <vlad.123jj@gmail.com>
2026-01-20 17:24:58 +0200

    add menu

---
+++ src/components/menu-component/MenuComponent.tsx
@@ -0,0 +1,30 @@
+import Link from "next/link";
+
+const MenuComponent = () => {
+    return (
+        <div>
+
+            <ul>
+                <li>
+                    <Link href = '/'>Home</Link>
+                </li>
+
+                <li>
+                    <Link href = '/users'>Users</Link>
+                </li>
+
+                <li>
+                    <Link href = '/posts'>Posts</Link>
+                </li>
+
+                <li>
+                    <Link href = '/comments'>Comments</Link>
+                </li>
+
+            </ul>
+
+        </div>
+    );
+};
+
+export default MenuComponent;
