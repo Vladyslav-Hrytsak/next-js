e54991919e9e6f0f984222248844e000dca3baa3
Vladyslav_Hrytsak <vlad.123jj@gmail.com>
2026-01-23 17:20:22 +0200

    homework 2

---
+++ src/app/add-cars/page.tsx
@@ -0,0 +1,11 @@
+import FormComponent from "@/components/form-component/FormComponent";
+
+const AddCarsPage = () => {
+    return (
+        <div>
+            <FormComponent/>
+        </div>
+    );
+};
+
+export default AddCarsPage;
